8950ecfb18b68df2d22da879157079b71fe89de2
cswanso2 <cswanso2@gelib-057-28.ews.illinois.edu>
2015-05-08 13:56:32 -0500

    fixed bug in isEmpty graphTests, added Tests for GraphTests

---
+++ jgrapht-core/src/test/java/org/jgrapht/experimental/alg/GraphTestsTest.java
@@ -0,0 +1,110 @@
+/* ==========================================
+ * JGraphT : a free Java graph-theory library
+ * ==========================================
+ *
+ * Project Info:  http://jgrapht.sourceforge.net/
+ * Project Creator:  Barak Naveh (http://sourceforge.net/users/barak_naveh)
+ *
+ * (C) Copyright 2003-2008, by Barak Naveh and Contributors.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+/* -----------------------------
+ * DefaultDirectedGraphTest.java
+ * -----------------------------
+ * (C) Copyright 2003-2008, by Barak Naveh and Contributors.
+ *
+ * Original Author:  Barak Naveh
+ * Contributor(s):   -
+ *
+ * $Id$
+ *
+ * Changes
+ * -------
+ * 09-Aug-2003 : Initial revision (BN);
+ *
+ */
+package org.jgrapht.graph;
+
+import java.util.*;
+
+import org.jgrapht.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.jgrapht.event.*;
+import org.jgrapht.graph.builder.*;
+import org.jgrapht.experimental.*;
+import org.jgrapht.alg.*;
+
+/**
+ * A unit test for directed multigraph.
+ *
+ * @author Barak Naveh
+ * @since Aug 9, 2003
+ */
+public class GraphTestsTest
+{
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+ 	* A TestSuite for the partite random graph generator
+ 	*
+ 	* @author Charles Swanson
+ 	* @since May 5th, 2015
+ 	*/
+
+ 	@Test
+    public void testIsEmpty()
+    {
+    	
+        UndirectedGraph<Integer, DefaultEdge> g = new SimpleGraph<Integer, DefaultEdge>(DefaultEdge.class);
+        assertEquals(true, GraphTests.isEmpty(g) );
+        int first = 1;
+        g.addVertex(first);
+        assertEquals(false, GraphTests.isEmpty(g) );
+    }
+    
+    @Test
+    public void testIsConnected()
+    {
+    	
+        UndirectedGraph<Integer, DefaultEdge> g = new SimpleGraph<Integer, DefaultEdge>(DefaultEdge.class);
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        g.addVertex(first);
+        g.addVertex(second);
+        g.addEdge(first, second);
+        assertEquals(true, GraphTests.isComplete(g) );
+        g.addVertex(third);
+        assertEquals(false, GraphTests.isComplete(g) );
+        g.addEdge(third, second);
+        g.addEdge(first, third);
+        assertEquals(true, GraphTests.isComplete(g) );
+    }
+
+	private static class IntegerVertexFactory
+        implements VertexFactory<Integer>
+    {
+        private int nextVertex = 0;
+
+        @Override
+        public Integer createVertex()
+        {
+            return nextVertex++;
+        }
+    }
+}
+
+// End DefaultDirectedGraphTest.java

918f7bc9019732334ca6b5cb4c57b28cc871dfa7
Brandon Holland <beholla2@illinois.edu>
2015-05-07 22:15:42 -0500

    Added more coverage for DirectedGraphUnion

--- jgrapht-core/src/test/java/org/jgrapht/graph/DirectedGraphUnionTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/graph/DirectedGraphUnionTest.java
@@ -1,5 +1,5 @@
 /**
-* UnmodifiableUndirectedGraph Test
+* DirectedGraphUnion Test
 *
 * Author: Brandon Holland
 *
@@ -24,67 +24,73 @@ public class DirectedGraphUnionTest
 {
 
         
-	//Test functionality of DirectedGraphUnion with WeightCombiner
+    //Test functionality of DirectedGraphUnion with WeightCombiner
     @Test
     public void testDirectedGraphUnionWCombiner()
     {
-	
-	DirectedGraph<String, Integer> one = new SimpleDirectedGraph<String, Integer>(Integer.class);                
+    
+    DirectedGraph<String, Integer> one = new SimpleDirectedGraph<String, Integer>(Integer.class);                
 
-	DirectedGraph<String, Integer> two = new SimpleDirectedGraph<String, Integer>(Integer.class);
+    DirectedGraph<String, Integer> two = new SimpleDirectedGraph<String, Integer>(Integer.class);
 
         String vertex1 = "1";
         String vertex2 = "2";
 
-	one.addVertex(vertex1);
-	two.addVertex(vertex2);
+    one.addVertex(vertex1);
+    two.addVertex(vertex2);
 
-	DirectedGraph<String, Integer> test = new DirectedGraphUnion(one, two, WeightCombiner.SUM);
+    DirectedGraph<String, Integer> test = new DirectedGraphUnion(one, two, WeightCombiner.SUM);
 
-	assertEquals(test.containsVertex(vertex1), true);
-	assertEquals(test.containsVertex(vertex2), true);
-    }	
+    assertEquals(test.containsVertex(vertex1), true);
+    assertEquals(test.containsVertex(vertex2), true);
+    }   
 
-	//Test functionality of DirectedGraphUnion
+    //Test functionality of DirectedGraphUnion
     @Test
     public void testDirectedGraphUnion()
     {
-	
-	DirectedGraph<String, Integer> one = new SimpleDirectedGraph<String, Integer>(Integer.class);                
+    
+    DirectedGraph<String, Integer> one = new SimpleDirectedGraph<String, Integer>(Integer.class);                
 
-	DirectedGraph<String, Integer> two = new SimpleDirectedGraph<String, Integer>(Integer.class);
+    DirectedGraph<String, Integer> two = new SimpleDirectedGraph<String, Integer>(Integer.class);
 
         String vertex1 = "1";
         String vertex2 = "2";
 
-	one.addVertex(vertex1);
-	two.addVertex(vertex2);
+    one.addVertex(vertex1);
+    two.addVertex(vertex2);
 
-	DirectedGraph<String, Integer> test = new DirectedGraphUnion(one, two);
+    DirectedGraph<String, Integer> test = new DirectedGraphUnion(one, two);
 
-	assertEquals(test.containsVertex(vertex1), true);
-	assertEquals(test.containsVertex(vertex2), true);
+    assertEquals(test.containsVertex(vertex1), true);
+    assertEquals(test.containsVertex(vertex2), true);
     }
 
     //Test inDegreeOf
     @Test
     public void testInDegreeOf()
     {
-    	DirectedGraph<String, Integer> g = new SimpleDirectedGraph<String, Integer>(Integer.class);
+        DirectedGraph<String, Integer> first = new SimpleDirectedGraph<String, Integer>(Integer.class);                
 
-    	String vertex1 = "1";
+        DirectedGraph<String, Integer> second = new SimpleDirectedGraph<String, Integer>(Integer.class);
+
+        String vertex1 = "1";
         String vertex2 = "2";
-        String vertex3 = "3";
+    String vertex3 = "3";
+    String vertex4 = "4";
 
-        Integer one = 1;
+        first.addVertex(vertex1);
+        first.addVertex(vertex2);
+    first.addVertex(vertex3);
+    second.addVertex(vertex4);
+
+    Integer one = 1;
         Integer two = 2;
+        
+        first.addEdge(vertex2, vertex1, one);
+        first.addEdge(vertex3, vertex1, two);
 
-        g.addVertex(vertex1);
-        g.addVertex(vertex2);
-        g.addVertex(vertex3);
-
-        g.addEdge(vertex2, vertex1, one);
-        g.addEdge(vertex3, vertex1, two);
+        DirectedGraph<String, Integer> g = new DirectedGraphUnion(first, second);
 
         assertEquals(g.inDegreeOf(vertex1), 2);
     }
@@ -93,21 +99,27 @@ public class DirectedGraphUnionTest
     @Test
     public void testOutDegreeOf()
     {
-    	DirectedGraph<String, Integer> g = new SimpleDirectedGraph<String, Integer>(Integer.class);
+        DirectedGraph<String, Integer> first = new SimpleDirectedGraph<String, Integer>(Integer.class);                
 
-    	String vertex1 = "1";
+        DirectedGraph<String, Integer> second = new SimpleDirectedGraph<String, Integer>(Integer.class);
+
+        String vertex1 = "1";
         String vertex2 = "2";
-        String vertex3 = "3";
+    String vertex3 = "3";
+    String vertex4 = "4";
 
-        Integer one = 1;
+        first.addVertex(vertex1);
+        first.addVertex(vertex2);
+    first.addVertex(vertex3);
+    second.addVertex(vertex4);
+
+    Integer one = 1;
         Integer two = 2;
+        
+        first.addEdge(vertex1, vertex2, one);
+        first.addEdge(vertex1, vertex3, two);
 
-        g.addVertex(vertex1);
-        g.addVertex(vertex2);
-        g.addVertex(vertex3);
-
-        g.addEdge(vertex1, vertex2, one);
-        g.addEdge(vertex1, vertex3, two);
+        DirectedGraph<String, Integer> g = new DirectedGraphUnion(first, second);
 
         assertEquals(g.outDegreeOf(vertex1), 2);
     }
@@ -116,23 +128,29 @@ public class DirectedGraphUnionTest
     @Test
     public void testIncomingEdgesOf()
     {
-    	DirectedGraph<String, Integer> g = new SimpleDirectedGraph<String, Integer>(Integer.class);
+        DirectedGraph<String, Integer> first = new SimpleDirectedGraph<String, Integer>(Integer.class);                
 
-    	Set<Integer> edgeSet;
+        DirectedGraph<String, Integer> second = new SimpleDirectedGraph<String, Integer>(Integer.class);
 
-    	String vertex1 = "1";
+    Set<Integer> edgeSet;
+
+        String vertex1 = "1";
         String vertex2 = "2";
-        String vertex3 = "3";
+    String vertex3 = "3";
+    String vertex4 = "4";
 
-        Integer one = 1;
+        first.addVertex(vertex1);
+        first.addVertex(vertex2);
+    first.addVertex(vertex3);
+    second.addVertex(vertex4);
+
+    Integer one = 1;
         Integer two = 2;
+        
+        first.addEdge(vertex2, vertex1, one);
+        first.addEdge(vertex3, vertex1, two);
 
-        g.addVertex(vertex1);
-        g.addVertex(vertex2);
-        g.addVertex(vertex3);
-
-        g.addEdge(vertex2, vertex1, one);
-        g.addEdge(vertex3, vertex1, two);
+        DirectedGraph<String, Integer> g = new DirectedGraphUnion(first, second);
 
         edgeSet = g.incomingEdgesOf(vertex1);
 
@@ -143,23 +161,29 @@ public class DirectedGraphUnionTest
     @Test
     public void testOutgoingEdgesOf()
     {
-    	DirectedGraph<String, Integer> g = new SimpleDirectedGraph<String, Integer>(Integer.class);
+        DirectedGraph<String, Integer> first = new SimpleDirectedGraph<String, Integer>(Integer.class);                
 
-    	Set<Integer> edgeSet;
+        DirectedGraph<String, Integer> second = new SimpleDirectedGraph<String, Integer>(Integer.class);
 
-    	String vertex1 = "1";
+    Set<Integer> edgeSet;
+
+        String vertex1 = "1";
         String vertex2 = "2";
-        String vertex3 = "3";
+    String vertex3 = "3";
+    String vertex4 = "4";
 
-        Integer one = 1;
+        first.addVertex(vertex1);
+        first.addVertex(vertex2);
+    first.addVertex(vertex3);
+    second.addVertex(vertex4);
+
+    Integer one = 1;
         Integer two = 2;
+        
+        first.addEdge(vertex1, vertex2, one);
+        first.addEdge(vertex1, vertex3, two);
 
-        g.addVertex(vertex1);
-        g.addVertex(vertex2);
-        g.addVertex(vertex3);
-
-        g.addEdge(vertex1, vertex2, one);
-        g.addEdge(vertex1, vertex3, two);
+        DirectedGraph<String, Integer> g = new DirectedGraphUnion(first, second);
 
         edgeSet = g.outgoingEdgesOf(vertex1);
 

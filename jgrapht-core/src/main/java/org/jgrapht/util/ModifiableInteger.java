f1371f41f2f7e2ca36a4bbd8cd9dd34f8b61a957
Brandon Holland <beholla2@illinois.edu>
2015-05-09 21:22:17 -0500

    Eliminate Unstability

--- jgrapht-core/src/main/java/org/jgrapht/util/ModifiableInteger.java
+++ jgrapht-core/src/main/java/org/jgrapht/util/ModifiableInteger.java
@@ -164,9 +164,9 @@ public class ModifiableInteger
         //Edited by Brandon Holland
         //BUG: From documentation, supposed to return +1 if anotherInteger is greater than testing ModifiableInteger
         //Return -1 if anotherInteger is less than testing ModifiableInteger
-        //return (thisVal < anotherVal) ? -1 : ((thisVal == anotherVal) ? 0 : 1);
+        return (thisVal < anotherVal) ? -1 : ((thisVal == anotherVal) ? 0 : 1);
 
-        return (thisVal < anotherVal) ? 1 : ((thisVal == anotherVal) ? 0 : -1);
+        //return (thisVal < anotherVal) ? 1 : ((thisVal == anotherVal) ? 0 : -1);
     }
 
     /**

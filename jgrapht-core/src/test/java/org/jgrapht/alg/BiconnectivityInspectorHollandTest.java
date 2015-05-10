bad7b47d433610087ab7bde6289c542dbb0a10fc
Brandon Holland <beholla2@illinois.edu>
2015-05-09 22:37:00 -0500

    Fixed Bugs from last commit

--- jgrapht-core/src/test/java/org/jgrapht/alg/BiconnectivityInspectorHollandTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/BiconnectivityInspectorHollandTest.java
@@ -14,6 +14,8 @@ import org.jgrapht.*;
 
 import org.jgrapht.util.*;
 
+import org.jgrapht.graph.*;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.Before;
@@ -23,9 +25,9 @@ import org.junit.After;
 public class BiconnectivityInspectorHollandTest
 {
     @Test
-    public void test()
+    public void testGetBiconnectedVertexComponents()
     {
-        UndirectedGraph<String, DefaultEdge> g = new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+        UndirectedGraph<String, Integer> g = new SimpleGraph<String, Integer>(Integer.class);
 
         String v1 = "v1";
         String v2 = "v2";
@@ -35,12 +37,34 @@ public class BiconnectivityInspectorHollandTest
 
         g.addVertex(v1);
         g.addVertex(v2);
-        g.addvertex(v3);
+        g.addVertex(v3);
 
         g.addEdge(v1,v2,one);
 
         BiconnectivityInspector inspector = new BiconnectivityInspector(g);
 
         assertEquals(inspector.getBiconnectedVertexComponents(v1).size(), 1);
+    }
+
+    @Test
+    public void testisBiconnected()
+    {
+        UndirectedGraph<String, Integer> g = new SimpleGraph<String, Integer>(Integer.class);
+
+        String v1 = "v1";
+        String v2 = "v2";
+        String v3 = "v3";
+
+        Integer one = 1;
+
+        g.addVertex(v1);
+        g.addVertex(v2);
+        g.addVertex(v3);
+
+        g.addEdge(v1,v2,one);
+
+        BiconnectivityInspector inspector = new BiconnectivityInspector(g);
+
+        assertEquals(inspector.isBiconnected(), true);
     }
 }
